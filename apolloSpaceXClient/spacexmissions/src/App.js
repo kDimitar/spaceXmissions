aba4e68b1d2c16c46bd808dc05b140109ce08ae6
kDimitar <k.dimitar93@gmail.com>
2021-11-26 20:13:54 +0100

    adding components,hooks, and router

--- apolloSpaceXClient/spacexmissions/src/App.js
+++ apolloSpaceXClient/spacexmissions/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { ApolloClient,  InMemoryCache,  ApolloProvider,  useQuery,  gql } from "@apollo/client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const client = new ApolloClient({
+    uri: 'https://api.spacex.land/graphql',
+    cache: new InMemoryCache(),
+});
+
+const App = () => (
+    <ApolloProvider client={client}>
+        <Routing />
+    </ApolloProvider>
+);
 
 export default App;
